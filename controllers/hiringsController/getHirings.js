ff133543c161dc2fa18d749a062188d88c55c9a5
Syed Faizan Ali <syedfaizanali1450@gmail.com>
2026-03-02 06:16:48 +0530

    Abstract fields

--- controllers/hiringsController/getHirings.js
+++ controllers/hiringsController/getHirings.js
@@ -5,7 +5,7 @@ import Job from '../../models/job_role.js';
 // @access  Private
 export const getHirings = async (req, res) => {
     try {
-        const roles = await Job.find({ status: "ACTIVE" });
+        const roles = await Job.find({ status: "ACTIVE" }, { _id: 0, created_by: 0, __v: 0, last_updated: 0, createdAt: 0, updatedAt: 0 });
         res.json(roles);
     } catch (error) {
         res.status(500).json({ message: error.message });
